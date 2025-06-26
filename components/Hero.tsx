89a1e2e88aadaf5e547a94be3eb38df86f3322fd
Ashraf-outsource <ashrafoutsourceorigin@gmail.com>
2025-06-27 00:28:39 +0500

    change the content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,13 +12,16 @@ export const Hero: React.FC = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <div className="grid md:grid-cols-2 gap-8 lg:gap-16 items-center">
           <div className="text-center md:text-left py-12 md:py-0">
-            <h1 className="text-4xl sm:text-5xl lg:text-5xl font-bold text-white leading-tight mb-6">
-              WELCOME TO 
-              <br/>
-              ALCHEMIST RESIN
+            <h1 className="text-4xl sm:text-5xl lg:text-4xl font-bold text-white leading-tight mb-5">
+              WELCOME TO ALCHEMIST RESIN
             </h1>
+            <h3 className="text-1xl sm:text-1xl lg:text-2xl font-semibold text-gold mb-4">
+              New York’s Epoxy Flooring & Concrete Coating Specialists
+            </h3>
             <p className="text-lg text-gray-300 mb-10 max-w-x2 mx-auto md:mx-0">
-              Our skilled team takes pride in delivering exceptional garage epoxy finishes that not only elevate the appearance of your space but also provide unmatched durability. At ALCHEMIST Resin, we understand the importance of protecting and enhancing your concrete surfaces, which is why we offer professional concrete sealing services. 
+
+              At Alchemist Resin, we deliver premium epoxy flooring and high-performance surface coatings for residential, commercial, and garage spaces across New York City. Whether you’re upgrading your home interior, modernizing a retail space, or finishing a garage, we combine durability, design, and expert craftsmanship to bring your vision to life.
+              We also offer professional concrete sealing services to protect and preserve your surfaces—defending against moisture, staining, and long-term wear.
             </p>
             <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 justify-center md:justify-start">
               <a href="#quote-form">
